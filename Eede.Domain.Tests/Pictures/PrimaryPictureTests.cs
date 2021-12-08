02b0fcecccf02f08103f757c5b84e3a70af23c31
arkfinn <ark_finn@hotmail.com>
2021-12-09 00:31:47 +0900

    Move PickColor method to Picture class

--- Eede.Domain.Tests/Pictures/PrimaryPictureTests.cs
+++ Eede.Domain.Tests/Pictures/PrimaryPictureTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using Eede.Domain.Positions;
+using NUnit.Framework;
+using System;
+using System.Drawing;
 
 namespace Eede.Domain.Pictures.Tests
 {
@@ -8,9 +11,35 @@ namespace Eede.Domain.Pictures.Tests
         [Test]
         public void 引数imageがnullでnewはできない()
         {
-            Assert.Throws<System.ArgumentNullException>(() =>
+            Assert.Throws<ArgumentNullException>(() =>
             {
                 var p = new Picture(null);
+            });
+        }
+
+        [Test]
+        public void PickColorTest()
+        {
+            using (var b = new Bitmap(10, 10))
+            {
+                b.SetPixel(1, 2, Color.DarkSeaGreen);
+                b.SetPixel(2, 3, Color.Beige);
+                var d = new Picture(b);
+                Assert.AreEqual(Color.DarkSeaGreen.ToArgb(), d.PickColor(new Position(1, 2)).ToArgb());
+                Assert.AreEqual(Color.Beige.ToArgb(), d.PickColor(new Position(2, 3)).ToArgb());
+            }
+        }
+
+        [Test]
+        public void PickColorの引数posはbitmapの範囲外を許容しない()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                using (var b = new Bitmap(10, 10))
+                {
+                    var d = new Picture(b);
+                    d.PickColor(new Position(10, 10));
+                }
             });
         }
     }

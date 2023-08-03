dea9c95d7bbd52a1352b1bc22ce33df3b61e5ee3
A-9 <ark_finn@hotmail.com>
2023-08-04 00:11:09 +0900

    refactor

--- Eede.Domain.Tests/ImageBlenders/RGBOnlyImageBlenderTests.cs
+++ Eede.Domain.Tests/ImageBlenders/RGBOnlyImageBlenderTests.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Eede.Domain.Tests.ImageBlenders
+namespace Eede.Domain.ImageBlenders
 {
     [TestFixture]
     public class RGBOnlyImageBlenderTests

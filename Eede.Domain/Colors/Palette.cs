d7a001f78c9bd4180d110c37a9c33c997901b54a
A-9 <ark_finn@hotmail.com>
2024-04-30 01:00:08 +0900

    Add Palette

---
+++ Eede.Domain/Colors/Palette.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Eede.Domain.Colors;
+
+public class Palette
+{
+    const int MAX_LENGTH = 256;
+    private ImmutableList<ArgbColor> Colors;
+
+    private Palette(ImmutableList<ArgbColor> colors)
+    {
+        if (colors.Count != MAX_LENGTH)
+        {
+            throw new InvalidOperationException(colors.Count.ToString());
+        }
+        Colors = colors;
+    }
+
+    public static Palette Create()
+    {
+        ArgbColor[] colorArray = new ArgbColor[MAX_LENGTH];
+        for (int i = 0; i < MAX_LENGTH; i++)
+        {
+            colorArray[i] = new ArgbColor(0, 0, 0, 0);
+        }
+        return new Palette(colorArray.ToImmutableList());
+    }
+
+    public ArgbColor Get(int index)
+    {
+        return Colors[index];
+    }
+
+    public Palette Set(int index, ArgbColor value)
+    {
+        return new Palette(Colors.SetItem(index, value));
+    }
+
+    public void ForEach(Action<ArgbColor, int> action)
+    {
+        foreach (var item in Colors.Select((value, index) => new { value, index }))
+        {
+            action.Invoke(item.value, item.index);
+        }
+    }
+}

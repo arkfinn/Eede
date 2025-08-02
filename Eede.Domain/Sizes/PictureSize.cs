0ce74b9f5e9bba5c4cc94c8dbb46aa4b8be6feb3
arkfinn <ark_finn@hotmail.com>
2025-08-03 03:03:59 +0900

    feat: PictureSizeクラスにドキュメンテーションコメントを追加
    - ContainsメソッドとSwapメソッドにそれぞれの機能を説明するXMLコメントを追加しました。

--- Eede.Domain/Sizes/PictureSize.cs
+++ Eede.Domain/Sizes/PictureSize.cs
@@ -18,11 +18,24 @@ namespace Eede.Domain.Sizes
             Height = height;
         }
 
+        /// <summary>
+        /// 指定された位置がこのサイズの範囲内にあるかどうかを確認します。
+        /// </summary>
+        /// <param name="pos">確認する位置。</param>
+        /// <returns>
+        /// 指定された位置がこのサイズの範囲内にある場合は <c>true</c>、それ以外の場合は <c>false</c>。
+        /// </returns>
         public bool Contains(Position pos)
         {
             return pos.X >= 0 && pos.Y >= 0 && pos.X < Width && pos.Y < Height;
         }
 
+        /// <summary>
+        /// 幅と高さの値を入れ替えた新しい <see cref="PictureSize"/> インスタンスを返します。
+        /// </summary>
+        /// <returns>
+        /// 幅が元の高さ、高さが元の幅に設定された <see cref="PictureSize"/> オブジェクトを返します。
+        /// </returns>
         public PictureSize Swap()
         {
             return new PictureSize(Height, Width);

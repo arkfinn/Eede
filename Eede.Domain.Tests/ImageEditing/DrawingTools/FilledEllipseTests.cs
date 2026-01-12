82ef32c9d4030095710b1e9823778192945ad727
arkfinn <ark_finn@hotmail.com>
2026-01-12 21:47:35 +0900

    feat(domain): Implement FilledEllipse tool

---
+++ Eede.Domain.Tests/ImageEditing/DrawingTools/FilledEllipseTests.cs
@@ -0,0 +1,36 @@
+using Eede.Domain.ImageEditing;
+using Eede.Domain.ImageEditing.Blending;
+using Eede.Domain.ImageEditing.DrawingTools;
+using Eede.Domain.Palettes;
+using Eede.Domain.SharedKernel;
+using Eede.Domain.Tests.Helpers;
+using NUnit.Framework;
+
+namespace Eede.Domain.Tests.ImageEditing.DrawingTools
+{
+    [TestFixture]
+    public class FilledEllipseTests
+    {
+        [Test]
+        public void DrawTest()
+        {
+            // Arrange
+            Picture src = PictureHelper.ReadBitmap(@"ImageEditing\DrawingTools\test\base.png");
+            DrawingBuffer buffer = new(src);
+            PenStyle penStyle = new(new DirectImageBlender(), new ArgbColor(255, 255, 0, 0), 1);
+            FilledEllipse tool = new();
+            
+            // DrawerTests uses (5, 5) to (9, 9) for fillEllipse1.png
+            PositionHistory pos = new(new Position(5, 5));
+            
+            // Act
+            DrawingBuffer startBuffer = tool.DrawStart(buffer, penStyle, pos, false);
+            PositionHistory endPos = pos.Update(new Position(9, 9));
+            DrawingBuffer endBuffer = tool.DrawEnd(startBuffer, penStyle, endPos, false);
+
+            // Assert
+            Picture expected = PictureHelper.ReadBitmap(@"ImageEditing\DrawingTools\test\fillEllipse1.png");
+            Assert.That(endBuffer.Fetch().CloneImage(), Is.EqualTo(expected.CloneImage()));
+        }
+    }
+}

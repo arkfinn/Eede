07759eaf590c40b49361aefe1bf0a7a307477a49
arkfinn <ark_finn@hotmail.com>
2026-01-12 20:03:28 +0900

    feat(domain): Implement FilledRectangle tool and refactor BoxDrawingTool

--- Eede.Domain/ImageEditing/DrawingTools/FilledRectangle.cs
+++ Eede.Domain/ImageEditing/DrawingTools/FilledRectangle.cs
@@ -2,11 +2,11 @@ using Eede.Domain.SharedKernel;
 
 namespace Eede.Domain.ImageEditing.DrawingTools;
 
-public record Rectangle : BoxDrawingTool
+public record FilledRectangle : BoxDrawingTool
 {
     protected override Picture Draw(Drawer drawer, PositionHistory positionHistory, bool isShift)
     {
         Position to = isShift ? CalculateShiftedPosition(positionHistory.Start, positionHistory.Now) : positionHistory.Now;
-        return drawer.DrawRectangle(positionHistory.Start, to);
+        return drawer.DrawFillRectangle(positionHistory.Start, to);
     }
 }

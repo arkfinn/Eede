140e1ba6ab8a56103d051f27976a63a4cefcd541
arkfinn <ark_finn@hotmail.com>
2026-02-17 07:51:15 +0900

    fix(conductor): Apply review suggestions for track 'ダイアログボタンの統一と共通コンポーネントの導入'

--- Eede.Presentation.Tests/Controls/DialogButtonsHitTestTests.cs
+++ Eede.Presentation.Tests/Controls/DialogButtonsHitTestTests.cs
@@ -9,6 +9,7 @@ using NUnit.Framework;
 using System.Windows.Input;
 using Moq;
 using System.Linq;
+using System;
 
 namespace Eede.Presentation.Tests.Controls;
 
@@ -24,14 +25,15 @@ public class DialogButtonsHitTestTests
         var target = new DialogButtons
         {
             PrimaryText = "OK",
-            PrimaryCommand = mockCommand.Object,
-            Width = 200,
-            Height = 100
+            PrimaryCommand = mockCommand.Object
         };
 
-        // テスト用のウィンドウに配置してレイアウトを確定させる
-        var window = new Window { Content = target };
+        // ウィンドウに配置してサイズを確定させる
+        var window = new Window { Content = target, Width = 400, Height = 400 };
         window.Show();
+
+        // レイアウト確定待ち
+        Avalonia.Threading.Dispatcher.UIThread.RunJobs();
 
         // ボタンを探す
         var primaryButton = target.GetVisualDescendants()
@@ -40,29 +42,26 @@ public class DialogButtonsHitTestTests
 
         Assert.That(primaryButton, Is.Not.Null, "Primary button not found in template.");
 
-        // ボタンの左上隅（文字から離れた場所）の座標を計算
-        var pointOnEdge = new Point(2, 2); 
-        
-        // PointerPressed
-        primaryButton.RaiseEvent(new PointerPressedEventArgs(
-            primaryButton,
-            new Pointer(0, PointerType.Mouse, true),
-            primaryButton,
-            pointOnEdge,
-            0,
-            new PointerPointProperties(RawInputModifiers.None, PointerUpdateKind.LeftButtonPressed),
-            KeyModifiers.None));
+        // バインディングとコマンドの状態を確認
+        Assert.That(primaryButton.Command, Is.EqualTo(mockCommand.Object), "Command should be bound to the button.");
+        Assert.That(primaryButton.IsEnabled, Is.True, "Button should be enabled.");
 
-        // PointerReleased
-        primaryButton.RaiseEvent(new PointerReleasedEventArgs(
-            primaryButton,
-            new Pointer(0, PointerType.Mouse, true),
-            primaryButton,
-            pointOnEdge,
-            0,
-            new PointerPointProperties(RawInputModifiers.None, PointerUpdateKind.LeftButtonReleased),
-            KeyModifiers.None,
-            MouseButton.Left));
+        // ボタンの左上隅（2, 2）の座標をウィンドウ座標に変換
+        var pointOnEdge = new Point(2, 2);
+        
+        // 直接イベントを発行するのではなく、HeadlessWindowExtensions の Mouse メソッド群を使用する
+        // 座標は Window に対する相対座標が必要
+        var windowPoint = primaryButton.TranslatePoint(pointOnEdge, window);
+        Assert.That(windowPoint, Is.Not.Null, "Could not translate point to window coordinates.");
+
+        // ヘッドレステストのシミュレーションを使用してクリック
+        // Avalonia.Headless の拡張メソッドを使用
+        window.MouseMove(windowPoint.Value);
+        window.MouseDown(windowPoint.Value, MouseButton.Left);
+        window.MouseUp(windowPoint.Value, MouseButton.Left);
+
+        // UIスレッドのジョブを処理してコマンド実行を待つ
+        Avalonia.Threading.Dispatcher.UIThread.RunJobs();
 
         // コマンドが実行されたか確認
         mockCommand.Verify(x => x.Execute(It.IsAny<object>()), Times.AtLeastOnce, "Command was not executed when clicking on button edge.");
